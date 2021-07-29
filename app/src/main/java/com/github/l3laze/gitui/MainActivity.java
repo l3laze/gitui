ecf6bb0678d461a59cbba91a4099df126f386a2e
Tom <l3l_aze@yahoo.com>
2021-07-29 03:58:22 -0500

    Actually handling permissions. Whoa.

--- app/src/main/java/com/github/l3laze/gitui/MainActivity.java
+++ app/src/main/java/com/github/l3laze/gitui/MainActivity.java
@@ -1,58 +1,115 @@
 package com.github.l3laze.gitui;
 
+import android.Manifest;
 import android.app.Activity;
+import android.view.Window;
+import android.widget.Toast;
 import android.os.Bundle;
+import android.os.Build;
 import android.os.Environment;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.view.Window;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
 import android.content.Context;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 
+import android.content.pm.PackageManager;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import com.github.l3laze.gitui.R;
+import java.lang.reflect.*;
+
 
 public class MainActivity extends Activity {
-    private static MainActivity instance;
-    private static WebView webView;
+  private static MainActivity instance;
+  private static WebView webView;
+  protected WebAppInterface webAppInterface;
+	protected final int STORAGE_PERM_CODE = 1;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+  @Override
+  protected void onCreate (Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_main);
+    requestWindowFeature(Window.FEATURE_NO_TITLE);
+    setContentView(R.layout.activity_main);
 
-        webView = (WebView) findViewById(R.id.webapp);
-        webView.setWebViewClient(new WebViewClient());
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().setDomStorageEnabled(true);
-        webView.addJavascriptInterface(new WebAppInterface(this), "Android");
+    webView = (WebView) findViewById(R.id.webapp);
+    webView.setWebViewClient(new WebViewClient());
+    webView.setWebChromeClient(new WebChromeClient());
+    webView.getSettings().setJavaScriptEnabled(true);
+    webView.getSettings().setDomStorageEnabled(true);
 
-        instance = this;
+    webAppInterface = new WebAppInterface(this);
+    webView.addJavascriptInterface(webAppInterface, "Android");
 
-        loadWebapp();
+    instance = this;
+
+    loadWebapp();
+  }
+
+  public void havePermission (int reqCode) {
+
+    if (reqCode == STORAGE_PERM_CODE) {
+      checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, STORAGE_PERM_CODE);
     }
+  }
 
-    public static MainActivity getInstance() {
-      return instance;
+  public static MainActivity getInstance () {
+		return instance;
+  }
+
+	public void checkPermission (String permission, int reqCode) {
+    if (ContextCompat.checkSelfPermission(MainActivity.this, permission) == PackageManager.PERMISSION_DENIED) { 
+      // Requesting the permission 
+      ActivityCompat.requestPermissions(MainActivity.this, new String[] { permission }, reqCode); 
+    } else {
+      savePermission(reqCode, true);
     }
-
-    public static void loadWebapp () {
-      String externalBase = Environment.getExternalStorageDirectory().getAbsolutePath() + "/gitui";
-      java.io.File external = new java.io.File(externalBase + "/index.html");
-      String internal = "file:///android_asset/gitui/index.html";
-      String url;
-
-      if (external.exists()) url = external.toString();
-      else url = internal;
-
-      webView.loadUrl(url);
+  }
+  
+  public void savePermission (int reqCode, boolean isAllowed) {
+    if (reqCode == STORAGE_PERM_CODE) {
+      MainActivity.getInstance().webAppInterface.storagePermission = isAllowed;
     }
+  }
 
-    protected void copyTextToClipboard (String text) {
-        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-        ClipData clip = ClipData.newPlainText("copied text", text);
-        clipboard.setPrimaryClip(clip);
+  @Override
+  public void onRequestPermissionsResult (int requestCode, String[] permissions, int[] grantResults) { 
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults); 
+
+    if (requestCode == STORAGE_PERM_CODE) { 
+      if (grantResults.length > 0
+          && grantResults[ 0 ] == PackageManager.PERMISSION_GRANTED) { 
+        Toast.makeText(MainActivity.this, "Storage Permission Granted", Toast.LENGTH_SHORT).show();
+        savePermission(STORAGE_PERM_CODE, true);
+      } else { 
+        Toast.makeText(MainActivity.this, "Storage Permission Denied", Toast.LENGTH_SHORT).show();
+        savePermission(STORAGE_PERM_CODE, false);
+      } 
     }
+  } 
+
+  public void loadWebapp () {
+    String externalBase = Environment.getExternalStorageDirectory().getAbsolutePath() + "/gitui";
+    java.io.File external = new java.io.File(externalBase + "/index.html");
+    String internal = "file:///android_asset/gitui/index.html";
+    String url;
+
+    if (external.exists() && external.canRead()) url = external.toString();
+    else url = internal;
+
+    webView.loadUrl(url);
+
+    MainActivity.getInstance().havePermission(STORAGE_PERM_CODE);
+  }
+
+  protected void copyTextToClipboard (String text) {
+    ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+    ClipData clip = ClipData.newPlainText("copied text", text);
+    clipboard.setPrimaryClip(clip);
+  }
 }
+
--- app/src/main/java/com/github/l3laze/gitui/WebAppInterface.java
+++ app/src/main/java/com/github/l3laze/gitui/WebAppInterface.java
@@ -16,177 +16,181 @@ import android.content.res.AssetManager;
 import android.os.Environment;
 
 
-
 public class WebAppInterface {
-  static Context mContext;
-  static AssetManager assetManager;
+	static Context mContext;
+	static AssetManager assetManager;
+  static boolean storagePermission = false;
 
-  WebAppInterface (Context c) {
-    mContext = c;
-  }
-
-  @JavascriptInterface
-  public static int androidVersion () {
-    return android.os.Build.VERSION.SDK_INT;
-  }
+	WebAppInterface (Context c) {
+		mContext = c;
+	}
 
   @JavascriptInterface
-  public static void showToast(String text) {
-    Toast.makeText(mContext, text, Toast.LENGTH_SHORT).show();
-  }
+  public static boolean havePermission () {
+    return storagePermission;
+	}
 
-  @JavascriptInterface
-  public static void copyToClipboard(String text) {
-    MainActivity.getInstance().copyTextToClipboard(text);
-  }
+	@JavascriptInterface
+	public static int androidVersion () {
+		return android.os.Build.VERSION.SDK_INT;
+	}
 
-  @JavascriptInterface
-  public static String normalize (String path) {
-    return Paths.get(path).normalize().toString();
-  }
+	@JavascriptInterface
+	public static void showToast (String text) {
+		Toast.makeText(mContext, text, Toast.LENGTH_SHORT).show();
+	}
 
-  @JavascriptInterface
-  public static  String relativize (String from, String to) {
-    return Paths.get(from).relativize(Paths.get(to)).toString();
-  }
+	@JavascriptInterface
+	public static void copyToClipboard (String text) {
+		MainActivity.getInstance().copyTextToClipboard(text);
+	}
 
-  @JavascriptInterface
-  public static String resolve (String path, String other) {
-    return Paths.get(path).resolve(other).toString();
-  }
+	@JavascriptInterface
+	public static String normalize (String path) {
+		return Paths.get(path).normalize().toString();
+	}
 
-  @JavascriptInterface
-  public static boolean isFile (String path) {
-    return new File(path).isFile();
-  }
- 
-  @JavascriptInterface
-  public static boolean isDir (String path) {
-    return new File(path).isFile();
-  }
+	@JavascriptInterface
+	public static  String relativize (String from, String to) {
+		return Paths.get(from).relativize(Paths.get(to)).toString();
+	}
 
-  @JavascriptInterface
-  public static Boolean fileExists (String path) {
-    return Files.exists(Paths.get(path));
-  }
- 
-  @JavascriptInterface
-  public static void makeDir (String path) {
-    try {
-      Files.createDirectories(Paths.get(path));
-    } catch (IOException ioe) {
-      ioe.printStackTrace();
-    }
-  }
- 
-  @JavascriptInterface
-  public static boolean removePath (String path) {
-    File p = new File(path);
-    File[] contents = p.listFiles();
- 
-    if (contents != null) {
-      for (File f : contents) {
-        removePath(f.toString());
-      }
-    }
- 
-    return p.delete();
-  }
+	@JavascriptInterface
+	public static String resolve (String path, String other) {
+		return Paths.get(path).resolve(other).toString();
+	}
 
-  @JavascriptInterface
-  public static String readDir (String path) {
-    StringBuilder dirList = new StringBuilder();
-    File d = new File(path);
-    File[] list = d.listFiles();
- 
-    for (int i = 0; i < list. length; i++) {
-       dirList.append("\n" + list[i].getName());
-    }
- 
-    return dirList.toString();
-  }
+	@JavascriptInterface
+	public static boolean isFile (String path) {
+		return new File(path).isFile();
+	}
 
-  @JavascriptInterface
-  public static void writeFile (String path, String data) {
-    try {
-      Files.write(Paths.get(path), data.getBytes());
-    } catch (IOException ioe) {
-      ioe.printStackTrace();
-    }
-  }
+	@JavascriptInterface
+	public static boolean isDir (String path) {
+		return new File(path).isFile();
+	}
 
-  @JavascriptInterface
-  public static String readFile (String path) {
-    String data;
- 
-    try {
-      data = new String (Files.readAllBytes(Paths.get(path)));
-    } catch (Exception e) {
-      e.printStackTrace();
-      data = "";
-    }
- 
-    return data;
-  }
+	@JavascriptInterface
+	public static Boolean fileExists (String path) {
+		return Files.exists(Paths.get(path)) && Files.isReadable(Paths.get(path));
+	}
 
-  @JavascriptInterface
-  public static String copyAssets(String path) {
-    File targetFolder;
+	@JavascriptInterface
+	public static void makeDir (String path) {
+		try {
+			Files.createDirectories(Paths.get(path));
+		}
+    catch (IOException ioe) {
+		}
+	}
 
-    try {
-      // Log.i(LOG_TAG, "Copying " + path);
-      // StringBuilder contents = new StringBuilder();
-      targetFolder = new File(Environment.getExternalStorageDirectory().getAbsolutePath());
-      assetManager = mContext.getAssets();
-      String sources[] = assetManager.list(path);
+	@JavascriptInterface
+	public static boolean removePath (String path) {
+		File p = new File(path);
+		File[] contents = p.listFiles();
 
-      if (sources.length == 0) {
-        // contents.append("Copying " + path + " @ ");
-        // contents.append(copyAssetFileToFolder(path, targetFolder) + " bytes");
-        copyAssetFileToFolder(path, targetFolder);
-      } else {
-          if (path.startsWith("images") || path.startsWith("sounds") || path.startsWith("webkit")) {
-            // Log.i(LOG_TAG, "  > Skipping " + path);
-          }
+		if (contents != null) {
+			for (File f : contents) {
+				removePath(f.toString());
+			}
+		}
 
-          File targetDir = new File(targetFolder, path);
-          targetDir.mkdirs();
-          if (!targetDir.exists()) throw new Exception("Grant storage permissions to create directories.");
+		return p.delete();
+	}
 
-          for (String source : sources) {
-            String fullSourcePath = path.equals("") ? source : (path + File.separator + source);
-            // contents.append(copyAssets(fullSourcePath) + "\n");
-            copyAssets(fullSourcePath);
-          }
-      }
+	@JavascriptInterface
+	public static String readDir (String path) {
+		StringBuilder dirList = new StringBuilder();
+		File d = new File(path);
+		File[] list = d.listFiles();
 
-      // return contents.toString().replace("\n\n", "\n");
-      return targetFolder.toString() + "/gitui";
-    } catch (Exception e) {
-      // return "error: " + e.toString();
-      return "";
-    }
-  }
+		for (int i = 0; i < list. length; i++) {
+			dirList.append("\n" + list[ i ].getName());
+		}
 
-  private static String copyAssetFileToFolder(String fullAssetPath, File targetBasePath) throws Exception {
-    InputStream in = assetManager.open(fullAssetPath);
-    File outFile = new File(targetBasePath, fullAssetPath);
-    OutputStream out = new FileOutputStream(outFile);
+		return dirList.toString();
+	}
 
-    byte[] buffer = new byte[16 * 1024];
-    int read;
-    long total = 0;
+	@JavascriptInterface
+	public static void writeFile (String path, String data) {
+		try {
+			Files.write(Paths.get(path), data.getBytes());
+		}
+    catch (IOException ioe) {
+		}
+	}
 
-    while ((read = in.read(buffer)) != -1) {
-      out.write(buffer, 0, read);
-      total += read;
-    }
+	@JavascriptInterface
+	public static String readFile (String path) {
+		String data;
 
-    in.close();
-    out.flush();
-    out.close();
+		try {
+			data = new String(Files.readAllBytes(Paths.get(path)));
+		}
+    catch (Exception e) {
+			data = "";
+		}
 
-    if (!outFile.exists()) throw new Exception("Grant storage permissions to read and write files.");
-    return "" + total;
-  }
+		return data;
+	}
+
+	@JavascriptInterface
+	public static String copyAssets (String path) {
+		File targetFolder;
+
+		try {
+			// Log.i(LOG_TAG, "Copying " + path);
+			// StringBuilder contents = new StringBuilder();
+			targetFolder = new File(Environment.getExternalStorageDirectory().getAbsolutePath());
+			assetManager = mContext.getAssets();
+			String sources[] = assetManager.list(path);
+
+			if (sources.length == 0) {
+				// contents.append("Copying " + path + " @ ");
+				// contents.append(copyAssetFileToFolder(path, targetFolder) + " bytes");
+				copyAssetFileToFolder(path, targetFolder);
+			} else {
+				if (path.startsWith("images") || path.startsWith("sounds") || path.startsWith("webkit")) {
+					// Log.i(LOG_TAG, "  > Skipping " + path);
+				}
+
+				File targetDir = new File(targetFolder, path);
+				targetDir.mkdirs();
+
+				for (String source : sources) {
+					String fullSourcePath = path.equals("") ? source : (path + File.separator + source);
+					// contents.append(copyAssets(fullSourcePath) + "\n");
+					copyAssets(fullSourcePath);
+				}
+			}
+
+			// return contents.toString().replace("\n\n", "\n");
+			return targetFolder.toString() + "/gitui";
+		}
+    catch (Exception e) {
+			// return "error: " + e.toString();
+			return "";
+		}
+	}
+
+	private static String copyAssetFileToFolder (String fullAssetPath, File targetBasePath) throws Exception {
+		InputStream in = assetManager.open(fullAssetPath);
+		File outFile = new File(targetBasePath, fullAssetPath);
+		OutputStream out = new FileOutputStream(outFile);
+
+		byte[] buffer = new byte[16 * 1024];
+		int read;
+		long total = 0;
+
+		while ((read = in.read(buffer)) != -1) {
+			out.write(buffer, 0, read);
+			total += read;
+		}
+
+		in.close();
+		out.flush();
+		out.close();
+
+		return "" + total;
+	}
 }
